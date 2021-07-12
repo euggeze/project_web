ffb4d836706cabfe1dff1ac15f3edaa2981791ee
euggeze <euggeze@gmail.com>
2021-07-12 14:06:17 +0300

    Added more static cases

--- documentation/html_prototype/scripts.js
+++ documentation/html_prototype/scripts.js
@@ -1,8 +1,16 @@
 function delete_func() {
     if (confirm("Are you sure want to delete this entry?")) {
-       //delete event
+       document.location.href = 'deleted_empl.html'
     }
     else{
        //undo delete 
+    }
+  }
+ function delete_func_dep() {
+    if (confirm("Are you sure want to delete this entry?")) {
+       document.location.href = 'deleted_dep.html'
+    }
+    else{
+       //undo delete
     }
   }
