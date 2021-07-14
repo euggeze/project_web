866278a075feea1ee35a3e404dcc1135bb47428a
euggeze <euggeze@gmail.com>
2021-07-14 12:43:55 +0300

    Added in application Django, created directory in project

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+def read_requiremets():
+    """
+    Function for reading project requirements.
+
+    return:
+        Massive project requirements
+    """
+    with open('requirements.txt') as file_req:
+        return [line for line in file_req.readlines()]
+
+
+setuptools.setup(
+    name = 'Department_application',
+    version = '0.1',
+    packages = setuptools.find_packages(),
+    description = 'Application for monitoring employees in departments',
+    install_requires = read_requiremets(),
+    python_requires = '>=3.7.9'
+)
