597387c2066634c6b4665e540c463c94f8bd6b97
euggeze <euggeze@gmail.com>
2021-07-16 10:34:59 +0300

    Updated django structure

--- department_project/department_project/urls.py
+++ department_project/department_project/urls.py
@@ -1,4 +1,4 @@
-"""department_app URL Configuration
+"""department_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
