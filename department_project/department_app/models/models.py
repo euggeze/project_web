bcf2946fb9fc363aa9c86887bd2bc38ff06a02b2
euggeze <euggeze@gmail.com>
2021-07-25 21:01:11 +0300

    Update .py files

--- department_project/department_app/models/models.py
+++ department_project/department_app/models/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 
 class Employee(models.Model):
-"Class for object structure Employee"
+    """Class for object structure Employee"""
     id_employee = models.AutoField(primary_key=True, editable=False)
     name = models.CharField(max_length=60,null=False)
     date_of_birthday = models.DateField(null=False)
@@ -11,6 +11,6 @@ class Employee(models.Model):
 
 
 class Department(models.Model):
-"Class for object structure Department"
+    """Class for object structure Department"""
     id_department = models.AutoField(primary_key=True, editable=False)
     name_department = models.CharField(max_length=60,null=False)
